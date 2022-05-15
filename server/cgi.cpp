f5b37a6b51bc98759201c463dc80c04abdab5530
Guest User <yassine.abdou.1998@gmail.com>
2022-05-15 21:11:17 +0100

    added cgi.cpp

--- server/cgi.cpp
+++ server/cgi.cpp
@@ -1,79 +1,75 @@
 #include <unistd.h>
 
-pid_t create_child(int *pfd, bool post)
-{
-	int pid;
-
-	if (pipe(pfd))
-		throw std::runtime_error("error: pipe()");
-	if (post)
-		if (pipe(pfd + 2))
-			throw std::runtime_error("error: pipe()");
-	pid = fork();
-	if (pid < 0)
-		throw std::runtime_error("error: fork()");
-	return (pid);
-}
-
-void exec_cgi(int *pfd, bool post)
-{
-	if (pid == 0)
-	{
-		if (dup2(pfd[1], 1) == -1)
-			throw std::runtime_error("error: dup2()");
-		if (post)
-			if (dup2(pfd[2], 0))
-				throw std::runtime_error("error: dup2()");
-		for (int i = 0; i < 4; i++)
-			close(pfd[i]);
-		if (execve())
-			throw std::runtime_error("error: execve()");
-		//exit
-	}
-}
-
-void get_response(int *pfd, bool post)
-{
-	close(pfd[1]);
-	if (post)
-	{
-		close(pfd[2]);
-		write(pfd[3]);
-		close(pfd[3]);
-	}
-	while
-	{
-		read(pfd[0]);
-	}
-	close(pfd[0]);
-}
-
-void set_environnement(IParseable &header)
+void set_environnement(IParseable &header, IParseable &conf)
 {
 	setenv("SERVER_SOFTWARE", "Webserv", 1);
 	setenv("GATEWAY_INTERFACE", "CGI/1.1", 1);
 	setenv("SERVER_PROTOCOL", "HTTP/1.1", 1);
-	setenv("REQUEST_METHOD", header[0]["methode"].str(), 1);
-	if (header[1].contains("User-Agent"))
-		setenv("HTTP_USER_AGENT",header[1]["User-Agent"].str(),1);
-	if (header[0]["query_string"])
+	setenv("REQUEST_METHOD", header[0]["METHOD"].str(), 1);
+	setenv("REQUEST_URI", header[0]["URI"].str(), 1);
+	setenv("SERVER_NAME", conf[0]["SERVER_NAME"].str(), 1);
+	setenv("SERVER_PORT", conf[0]["SERVER_PORT"].str(), 1);
+	if (header[0]["QUERY_STRING"])
 		setenv("QUERY_STRING",header[1]["query_string"].str(),1);
 	if (header[1].contains("content_type"))
 		setenv("CONTENT_TYPE",header[1]["content_type"].str(),1);
 	if (header[1].contains("content_length"))
 		setenv("CONTENT_LENGTH",header[1]["content_length"].str(),1);
-	if (header[1].contains("script_name"))
-		setenv("SCRIPT_NAME",header[1]["content_length"].str(),1);
+	//if (header[1].contains("SCRIPT_NAME"))
+	//	setenv("SCRIPT_NAME",header[1]["SCRIPT_NAME"].str(),1);
+	//if (header[1].contains("PATH_INFO"))
+	//	setenv("PATH_INFO",header[0]["PAHT_INFO"].str(),1);
+	if (header[1].contains("HTTP_COOKIE"))
+		setenv("HTTP_COOKIE",header[1]["COOKIE"].str(),1);
+	if (header[1].contains("HTTP_ACCEPT"))
+		setenv("HTTP_ACCEPT",header[1]["ACCEPT"].str(),1);
+	if (header[1].contains("USER_AGENT"))
+		setenv("HTTP_USER_AGENT",header[1]["USER_AGENT"].str(),1);
+	if (header[1].contains("ACCEPT_LANGUAGE"))
+		setenv("HTTP_ACCEPT_LANGUAGE",header[1]["ACCEPT_LANGUAGE"].str(),1);
 }
 
-void launch_cgi(IParseable &header, FILE *body)
+int launch_cgi(IParseable &header, IParseable &loc, std::string &filename, const char *path)
 {
-	extern char		**environ;
-	int 			pfd[4];
-	pid_t			pid;
+	try
+	{
+		pid_t			pid;
+		int 			pfd[2];
+		int			fd_body;
+		const char		*arg[3];
+		extern char		**environ;
 
-	set_environnement();
-	pid = create_child(pfd, false);
-	exec_cgi();
-	get_response();
+		arg[0] = path;
+		arg[1] = header[0]["URI"].str().c_str();
+		arg[2] = NULL;
+
+		if (pipe(pfd) == -1)
+			throw std::runtime_error("error: pipe()");
+		if ( (pid = fork()) == -1 )
+			throw std::runtime_error("error: fork()");
+		if (pid == 0)
+		{
+			set_environnement();
+			if (header[0]["METHOD"].str() == "POST")
+			{
+				fd_body = open(filename.c_str(), O_RDONLY);
+				if (fd_body == -1)
+					throw std::runtime_error("error: launch_cgi() : open()");
+				if (fd_body > 2 && (dup2(fd_body, 0) == -1 || close(fd_body)))
+					throw std::runtime_error("error: launch_cgi() : dup2() or close()");
+			}
+			if (dup2(pfd[1], 1) == -1 || close(pfd[1]) == -1 || close(pfd[0]) == -1)
+				throw std::runtime_error("error: launch_cgi(): dup2() or close()");
+			if (chdir(path) == =1 || execve(arg[0], arg, environ) == -1)
+				throw std::runtime_error("error: launch_cgi() : chdir() or execve()");
+		}
+		if (close(pfd[1]) == -1)
+			throw std::runtime_error("error: launch_cgi() : close()");	
+		return (pfd[0]);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+		return (-1);
+	}
 }
